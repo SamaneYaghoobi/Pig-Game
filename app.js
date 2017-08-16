d3fa2fd9e2ff43ffc6e0d696f3511214b33abde1
Samane <baran.s2s.26.75@gmail.com>
2017-08-17 02:15:11 +0430

    Add files via upload

--- app.js
+++ app.js
@@ -1,18 +1,6 @@
 var scores, roundScore, activePlayer;
-
-scores = [0, 0];
-roundScore = 0;
-activePlayer = 0;
-
-
-document.querySelector('#score-0').textContent = '0';
-document.querySelector('#score-1').textContent = '0';
-document.querySelector('#current-0').textContent = '0';
-document.querySelector('#current-1').textContent = '0';
 var diceDOM = document.querySelector('.dice');
-diceDOM.style.display = 'none';
-
-
+init();
 
 document.querySelector('.btn-roll').addEventListener('click', function () {
     var dice = Math.floor(Math.random() * 6) + 1;
@@ -24,24 +12,44 @@ document.querySelector('.btn-roll').addEventListener('click', function () {
         document.querySelector('#current-' + activePlayer).textContent = roundScore;
     } else
         nextPlayer();
-})
+});
 
 document.querySelector('.btn-hold').addEventListener('click', function () {
     scores[activePlayer] += roundScore;
     document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+    
     if (scores[activePlayer] >= 20) {
         diceDOM.style.display = 'none';
         document.querySelector('#name-' + activePlayer).textContent = 'WINNER!';
         document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
         document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
         document.querySelector('#current-' + activePlayer).textContent = '0';
-
-
     } else
         nextPlayer();
 
-})
+});
 
+document.querySelector('.btn-new').addEventListener('click',init);
+
+function init()
+{
+    scores = [0, 0];
+    roundScore = 0;
+    activePlayer = 0;
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('winner');
+    document.querySelector('.player-0-panel').classList.remove('active');
+    document.querySelector('.player-1-panel').classList.remove('active');
+    document.querySelector('.player-0-panel').classList.add('active');
+    document.getElementById('name-1').textContent='Player 2';
+    document.getElementById('name-0').textContent='Player 1';
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+    document.getElementById('score-0').textContent = '0';
+    document.getElementById('score-1').textContent = '0';
+    diceDOM.style.display = 'none';
+    
+}
 
 function nextPlayer() {
     roundScore = 0;
