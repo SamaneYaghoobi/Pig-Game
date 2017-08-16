c81c365f6d8602cb6cd3cfb680355e4215bcaf2c
Samane <baran.s2s.26.75@gmail.com>
2017-08-17 01:07:08 +0430

    Add files via upload

--- app.js
+++ app.js
@@ -9,12 +9,38 @@ document.querySelector('#score-0').textContent = '0';
 document.querySelector('#score-1').textContent = '0';
 document.querySelector('#current-0').textContent = '0';
 document.querySelector('#current-1').textContent = '0';
-document.querySelector('.dice').style.display = 'none';
+var diceDOM = document.querySelector('.dice');
+diceDOM.style.display='none';
+
+
 
 document.querySelector('.btn-roll').addEventListener('click', function () {
     var dice = Math.floor(Math.random() * 6) + 1;
-    var diceDOM = document.querySelector('.dice');
     diceDOM.style.display = 'block';
     diceDOM.src = 'dice-' + dice + '.png';
-    document.querySelector('#current-' + activePlayer).textContent += dice;
+
+    if (dice !== 1) {
+        roundScore += dice;
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+    } else 
+        nextPlayer();
 })
+
+document.querySelector('.btn-hold').addEventListener('click', function () {
+    scores[activePlayer]+=roundScore;
+    document.querySelector('#score-'+activePlayer).textContent=scores[activePlayer];
+    nextPlayer();
+
+})
+
+
+function nextPlayer()
+{
+    roundScore = 0;
+    document.querySelector('#current-' + activePlayer).textContent = roundScore;
+    activePlayer === 1 ? activePlayer = 0 : activePlayer = 1;
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+    diceDOM.style.display = 'none';
+}
+
